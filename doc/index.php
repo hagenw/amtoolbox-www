7254c0c3b09ab4935769be7fe6098076a0b88068
Peter L. Soendergaard <soender@users.sourceforge.net>
2010-06-14 16:09:03 +0200

    added more dirs to doc/index.php

--- doc/index.php
+++ doc/index.php
@@ -14,6 +14,9 @@ The pages linked below are generated from the documentation in the M-files. This
 <ul>
 <li><a href="general/Contents.php">general</a> General routines.</li>
 <li><a href="filters/Contents.php">filters</a> Filters.</li>
+<li><a href="binaural/Contents.php">binaural</a> Binaural models.</li>
+<li><a href="humandata/Contents.php">human data</a> Human data.</li>
+<li><a href="experiments/Contents.php">experiments</a> Experiments.</li>
 <li><a href="examples/Contents.php">demos</a> Demos.</li>
 <li><a href="signals/Contents.php">signals</a> Test signals.</li>
 </ul>
