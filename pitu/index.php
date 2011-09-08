789638a5ad018481200e44c2e62cc167cacc4b0c
Peter L. Soendergaard <soender@users.sourceforge.net>
2011-09-08 10:15:42 +0200

    added pitu mockup

---
+++ pitu/index.php
@@ -0,0 +1,29 @@
+<!doctype html public "-//W3C//DTD html 4.0//EN">
+<html><head>
+<meta name="description"
+      content="PITU">
+
+<meta name="keywords"
+      content="auditory, modelling, toolbox, matlab">
+<meta name="alias" content="http://amtoolbox.sourceforge.net/">
+<title>PITU</title>
+<link rel="Bookmark" title="PITU Home Page" href="index.php">
+<link rel="Contents" href="index.php">
+<link rel="STYLESHEET" href="amtoolbox.css" type="text/css">
+</head>
+<body TEXT="#000000" BGCOLOR="#FFFFFF">
+
+   <?require("topnav.php");?>
+
+<table><tr valign=top><td>
+<div class="sidebar">
+   <?php require("/home/groups/a/am/amtoolbox/htdocs/doc/docnav.php");?>
+</div></td><td>
+
+<H2>Introduction</H2>
+This page is under construction.
+
+   <?require("footer.php");?>
+
+</body>
+</html>
--- topnav.php
+++ topnav.php
@@ -6,6 +6,7 @@
    <td><a href="mailto:amtoolbox-help@lists.sourceforge.net">Contact</a></td>
    <td><a href="/doc">Documentation</a></td>
    <td><a href="/notes">Notes</a></td>
+   <td><a href="/pitu">PITU</a></td>
    <td><a href="http://sourceforge.net/projects/amtoolbox">Development page</a></td>
 </tr>
 </table>
