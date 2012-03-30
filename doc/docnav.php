487ae5de2e35cca7fa4340c02c9449ea013a4037
Peter L. Soendergaard <soender@users.sourceforge.net>
2012-03-30 13:20:54 +0200

    added hrtf dir to docnav

--- doc/docnav.php
+++ doc/docnav.php
@@ -6,6 +6,7 @@
 <a href="/doc/monaural/Contents.php">monaural</a><br>
 <a href="/doc/binaural/Contents.php">binaural</a><br>
 <a href="/doc/humandata/Contents.php">human data</a><br>
+<a href="/doc/hrtf/Contents.php">HRTF</a><br>
 <a href="/doc/experiments/Contents.php">experiments</a><br>
 <a href="/doc/demos/Contents.php">demos</a><br>
 <a href="/doc/signals/Contents.php">signals</a><br>
