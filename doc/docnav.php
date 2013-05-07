d7b8481a08b3c9bf3d77d5acc3f7dc6bf36257d5
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-05-07 18:40:17 +0200

    Added bootstrap CSS style to the site

---
+++ doc/docnav.php
@@ -0,0 +1,17 @@
+<ul class="nav nav-list bs-docs-sidenav affix-top">
+  <li><b>Documentation:</b></li>
+  <li><a href="/doc/index.php">startup</a></li>
+  <li><a href="/doc/general/index.php">general</a></li>
+  <li><a href="/doc/filters/index.php">filters</a></li>
+  <li><a href="/doc/modelstages/index.php">model stages</a></li>
+  <li><a href="/doc/monaural/index.php">monaural</a></li>
+  <li><a href="/doc/binaural/index.php">binaural</a></li>
+  <li><a href="/doc/speech/index.php">speech</a></li>
+  <li><a href="/doc/humandata/index.php">human data</a></li>
+  <li><a href="/doc/hrtf/index.php">HRTF</a></li>
+  <li><a href="/doc/experiments/index.php">experiments</a></li>
+  <li><a href="/doc/demos/index.php">demos</a></li>
+  <li><a href="/doc/signals/index.php">signals</a></li>
+</ul>
+
+<!-- vim: set textwidth=120 ts=2 sw=2: -->
