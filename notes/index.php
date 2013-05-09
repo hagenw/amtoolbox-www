8d044d2fc6792c833105ec736d91377f2e855d34
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-05-09 11:28:01 +0200

    Updated notes/index.php

--- notes/index.php
+++ notes/index.php
@@ -1,29 +1,62 @@
 <!doctype html public "-//W3C//DTD html 4.0//EN">
-<html><head>
-<meta http-equiv="content-type" content="text-html; charset=utf-8">
-<meta name="description"
-      content="AMToolbox note series">
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description"
+    content="The Auditory Modelling Toolbox">
+  <meta name="keywords"
+    content="auditory, modelling, toolbox, matlab">
+  <meta name="alias" content="http://amtoolbox.sourceforge.net/">
+  <title>Auditory Modelling Toolbox</title>
+  <link rel="Bookmark" title="AMToolbox Home Page" href="/index.php">
+  <link rel="Contents" href="/index.php">
+  <!-- Styles -->
+  <!--<link rel="STYLESHEET" href="include/amtoolbox.css" type="text/css">-->
+  <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="/css/style.css" type="text/css">
+  <link rel="stylesheet" href="/css/bootstrap-responsive.min.css" type="text/css">
 
-<meta name="keywords"
-      content="auditory, modelling, binaural, preprint">
-<meta name="alias" content="http://amt.sourceforge.net/notes/">
-<title>AMToolbox Notes</title>
-<link rel="Bookmark" title="AMToolbox Notes" href="index.php">
-<link rel="Contents" href="index.php">
-<link rel="STYLESHEET" href="../include/amtoolbox.css" type="text/css">
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
 </head>
 
 <body>
-<div id="page">
 
-<?include("../include/topmenu.php");?>
 
-<?include("description.php");?>
+  <div class="container">
+    
+    <!-- nav header -->
+    <div class="masthead">
+      <?include("../include/topmenu.php");?>
+    </div>
 
-<?include("by_number.php");?>
+    <!-- main page -->
+    <div class="row-fluid">
+      <div class="span2">
+      </div><!--</span2>-->
 
-<?include("../include/footer.php");?>
+      <div class="span8">
+          
+        <br/>
+        <?include("description.php");?>
+
+        <!-- Table with entries -->
+        <?include("by_number.php");?>
+      </div><!--</span8>-->
+
+      <div class="span2">
+      </div><!--</span2>-->
+
+    </div><!--</row-fluid>-->
+
+    <?include("../footer.php");?>
+
+  </div><!--</container>-->
 
 </div>
 </body>
 </html>
+<!-- vim: set textwidth=120 ts=2 sw=2: -->
