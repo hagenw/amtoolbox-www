13f55614bb3b8a4a328ed566f0552e3eff38c055
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-05-09 11:46:09 +0200

    Updated the Pitu index.php

--- pitu/index.php
+++ pitu/index.php
@@ -1,58 +1,102 @@
 <!doctype html public "-//W3C//DTD html 4.0//EN">
-<html><head>
-<meta name="description"
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description"
       content="PITU">
-
-<meta name="keywords"
+  <meta name="keywords"
       content="danish, speech corpus,nonsense syllable">
-<meta name="alias" content="http://amtoolbox.sourceforge.net/">
-<title>PITU</title>
-<link rel="Bookmark" title="PITU Home Page" href="index.php">
-<link rel="Contents" href="index.php">
-<link rel="STYLESHEET" href="../include/amtoolbox.css" type="text/css">
+  <meta name="alias" content="http://amtoolbox.sourceforge.net/">
+  <title>PITU</title>
+  <link rel="Bookmark" title="PITU Home Page" href="/index.php">
+  <link rel="Contents" href="/index.php">
+  <!-- Styles -->
+  <!--<link rel="STYLESHEET" href="include/amtoolbox.css" type="text/css">-->
+  <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="/css/style.css" type="text/css">
+  <link rel="stylesheet" href="/css/bootstrap-responsive.min.css" type="text/css">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
 </head>
-<body  id="page">
 
-<?include("../include/topmenu.php");?>
-
-<p>
-
-This is the homepage for the  Pitu Danish nonsense syllable project.
-
-<h2>Introduction to the speech material</h2>
-
-The material consists of recordings of nonsense syllables spoken by 27 native talkers of Danish made in an anechoic chamber.
-17 Danish consonants were used [ptkbdgfsvmnrlhjSW]  (notation described in (Henrichsen, 2007),  special version of SAMPA). One of three vowels [iau]  followed the initial consonant (vowel [a] = [z] in the notation of Henrichsen (2007)).
-A second syllable [tu] followed the CV-syllables in order to ensure that no token was "accidentally" a Danish word.
-Each syllable was repeated three times following the standard intonation pattern for Danish (falling F0).
-Please refer to the documentation for further details.<p>
-
-An introduction to the speech corpus can be found in this <a href="http://server.elektro.dtu.dk/ftp/tuc/Pitu/Backgrnd.pdf">pdf</a> file.
+<body>
 
 
-<h2>Use of the material</h2>
+  <div class="container">
+    
+    <!-- nav header -->
+    <div class="masthead">
+      <?include("../include/topmenu.php");?>
+    </div>
 
-The material is intended for scientific use. Please feel free to use the it any way you like. It is licensed under the <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0</a>
-You can help the project by notifying us about any research you publish using this material. Please send an email to "tuc at elektro.dtu.dk".
+    <!-- main page -->
+    <div class="row-fluid">
+      <div class="span2">
+      </div><!--</span2>-->
 
-The idea is that as more and more data about the material is collected it is summarised on this website.<p>
+      <div class="span8">
+          
+        <br/>
+        <p>This is the homepage for the  Pitu Danish nonsense syllable project.</p>
 
-Please click <a href="http://server.elektro.dtu.dk/ftp/tuc/Pitu/">this link</a> to go to the Download cite were both sound and additional documentation can be found. The directory and file name structure is explained in the <a href="http://server.elektro.dtu.dk/ftp/tuc/Pitu/readme.txt">readme.txt</a> file.
+        <h3>Introduction to the speech material</h3>
 
-<h2>References</h2>
+        <p>The material consists of recordings of nonsense syllables spoken by 27 native talkers of Danish made in an anechoic chamber.
+        17 Danish consonants were used [ptkbdgfsvmnrlhjSW]  (notation described in (Henrichsen, 2007),  special version of SAMPA).
+        One of three vowels [iau]  followed the initial consonant (vowel [a] = [z] in the notation of Henrichsen (2007)).
+        A second syllable [tu] followed the CV-syllables in order to ensure that no token was "accidentally" a Danish word.
+        Each syllable was repeated three times following the standard intonation pattern for Danish (falling F0).
+        Please refer to the documentation for further details.</p>
 
-   <p>T.U. Christiansen, P.J. Henrichsen,
-"Objective Evaluation of Consonant-Vowel pairs produced by Native Speakers of Danish"
-   <em>Proceedings of Forum Acusticum 2011</em> (ISBN: 978-84-694-1520-7), 2011
-Presented at: Forum Acusticum 2011, 2011, Aalborg, Denmark</p>
-
-<p>M.L. Jepsen, T. Dau
-"Confusion of Danish consonants in white noise":
-   <em>Proceedings of ISAAR 2011: Speech perception and auditory disorders, 3rd International Symposium on Auditory and Audiological Research</em>, August 2011, Nyborg, Denmark</p>
+        <p>An introduction to the speech corpus can be found in this
+        <a href="http://server.elektro.dtu.dk/ftp/tuc/Pitu/Backgrnd.pdf">pdf</a> file.</p>
 
 
-<?include("../../include/footer.php");?>
+        <h3>Use of the material</h3>
+
+        <p>The material is intended for scientific use. Please feel free to use the it any way you like.
+        It is licensed under the
+        <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0</a>
+        You can help the project by notifying us about any research you publish using this material.
+        Please send an email to "tuc at elektro.dtu.dk".</p>
+
+        <p>The idea is that as more and more data about the material is collected it is summarised on this website.</p>
+
+        <p>Please click <a href="http://server.elektro.dtu.dk/ftp/tuc/Pitu/">this link</a> to go to the Download site were
+        both sound and additional documentation can be found. The directory and file name structure is explained in the
+        <a href="http://server.elektro.dtu.dk/ftp/tuc/Pitu/readme.txt">readme.txt</a> file.</p>
+
+        <h3>References</h3>
+
+        <p>T.U. Christiansen, P.J. Henrichsen,
+          "Objective Evaluation of Consonant-Vowel pairs produced by Native Speakers of Danish"
+          <em>Proceedings of Forum Acusticum 2011</em> (ISBN: 978-84-694-1520-7), 2011
+          Presented at: Forum Acusticum 2011, 2011, Aalborg, Denmark
+        </p>
+
+        <p>M.L. Jepsen, T. Dau
+          "Confusion of Danish consonants in white noise":
+          <em>Proceedings of ISAAR 2011: Speech perception and auditory disorders, 3rd International
+          Symposium on Auditory and Audiological Research</em>, August 2011, Nyborg, Denmark
+        </p>
+
+      </div><!--</span8>-->
+
+      <div class="span2">
+      </div><!--</span2>-->
+
+    </div><!--</row-fluid>-->
+
+    <!-- footer -->
+    <?include("../include/footer.php");?>
+
+  </div><!--</container>-->
 
 </div>
 </body>
 </html>
+<!-- vim: set textwidth=120 ts=2 sw=2: -->
