4ab960f22c45307796df079cd34b604f01b059cf
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-05-13 15:09:52 +0200

    Fixed the links in the docnav menu

--- doc/docnav.php
+++ doc/docnav.php
@@ -1,17 +1,17 @@
 <ul class="nav nav-list doc-sidenav affix-top">
   <li><a href=""><b>M-files</b></a></li>
-  <li><a href="/doc/startup.php"><i class="icon-chevron-right"></i>startup</a></li>
-  <li><a href="/doc/general/index.php"><i class="icon-chevron-right"></i>general</a></li>
-  <li><a href="/doc/filters/index.php"><i class="icon-chevron-right"></i>filters</a></li>
-  <li><a href="/doc/modelstages/index.php"><i class="icon-chevron-right"></i>model stages</a></li>
-  <li><a href="/doc/monaural/index.php"><i class="icon-chevron-right"></i>monaural</a></li>
-  <li><a href="/doc/binaural/index.php"><i class="icon-chevron-right"></i>binaural</a></li>
-  <li><a href="/doc/speech/index.php"><i class="icon-chevron-right"></i>speech</a></li>
-  <li><a href="/doc/humandata/index.php"><i class="icon-chevron-right"></i>human data</a></li>
-  <li><a href="/doc/hrtf/index.php"><i class="icon-chevron-right"></i>HRTF</a></li>
-  <li><a href="/doc/experiments/index.php"><i class="icon-chevron-right"></i>experiments</a></li>
-  <li><a href="/doc/demos/index.php"><i class="icon-chevron-right"></i>demos</a></li>
-  <li><a href="/doc/signals/index.php"><i class="icon-chevron-right"></i>signals</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/startup.php"><i class="icon-chevron-right"></i>startup</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/general/index.php"><i class="icon-chevron-right"></i>general</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/filters/index.php"><i class="icon-chevron-right"></i>filters</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/modelstages/index.php"><i class="icon-chevron-right"></i>model stages</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/monaural/index.php"><i class="icon-chevron-right"></i>monaural</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/binaural/index.php"><i class="icon-chevron-right"></i>binaural</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/speech/index.php"><i class="icon-chevron-right"></i>speech</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/humandata/index.php"><i class="icon-chevron-right"></i>human data</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/hrtf/index.php"><i class="icon-chevron-right"></i>HRTF</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/experiments/index.php"><i class="icon-chevron-right"></i>experiments</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/demos/index.php"><i class="icon-chevron-right"></i>demos</a></li>
+  <li><a href="<?php baseurl(); ?>/doc/signals/index.php"><i class="icon-chevron-right"></i>signals</a></li>
 </ul>
 
 <!-- vim: set textwidth=120 ts=2 sw=2: -->
