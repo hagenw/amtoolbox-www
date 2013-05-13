dcb6ec9507c1942495249b02068eeaf0fc5e61a5
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-05-13 14:35:55 +0200

    Added extra header file

---
+++ header.php
@@ -0,0 +1,26 @@
+<!doctype html public "-//W3C//DTD html 4.0//EN">
+<?php include("functions.php"); ?>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description"
+    content="The Auditory Modelling Toolbox">
+  <meta name="keywords"
+    content="auditory, modelling, toolbox, matlab">
+  <meta name="alias" content="http://amtoolbox.sourceforge.net/">
+  <title>Auditory Modelling Toolbox</title>
+  <link rel="Bookmark" title="AMToolbox Home Page" href="<?php baseurl();?>">
+  <link rel="Contents" href="<?php baseurl();?>">
+  <!-- Styles -->
+  <!--<link rel="STYLESHEET" href="include/amtoolbox.css" type="text/css">-->
+  <link rel="stylesheet" href="<?php baseurl();?>/css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="<?php baseurl();?>/css/style.css" type="text/css">
+  <link rel="stylesheet" href="<?php baseurl();?>/css/bootstrap-responsive.min.css" type="text/css">
+
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
