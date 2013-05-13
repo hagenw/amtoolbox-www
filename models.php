a568bde62d487b97ffb501bffdaa26853c5be267
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-05-13 09:59:35 +0200

    Added human data and sort entries in models.php

--- models.php
+++ models.php
@@ -62,6 +62,7 @@
           </thead>
           <tbody>
 
+            <!-- ------------ P R E P R O C E S S I N G -------------------- -->
             <tr class="info">
               <td><strong>Pre-processing</strong></td>
               <td></td>
@@ -209,10 +210,32 @@
                 breebaart2001a
               </td>
             </tr>
+
+            <!-- ------------ M O N A U R A L ---------------------------- -->
             <tr class="info">
               <td><strong>Monaural models</strong></td>
               <td></td>
               <td></td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Baumgartner model</strong> <br/>
+                Model for localization in saggital planes
+              </td>
+              <td>
+                baumgartner2013.m <br/>
+                plotbaumgartner2013.m <br/>
+                demo_baumgartner2013.m <br/>
+                exp_baumgartner2013.m <br/>
+                data_baumgartner2013.m
+              </td>
+              <td>
+                baumgartner2013assessment <br/>
+                baumgartner2012modelling <br/>
+                langendijk2002contribution <br/>
+                patterson1988efficient <br/>
+                dau1996qmeI
+              </td>
             </tr>
             <tr>
               <td>
@@ -244,18 +267,6 @@
             </tr>
             <tr>
               <td>
-                <strong>Viemeister model</strong> <br/>
-                Leaky-integrator model
-              </td>
-              <td>
-                viemeister79.m
-              </td>
-              <td>
-                ???
-              </td>
-            </tr>
-            <tr>
-              <td>
                 <strong>Langendijk model</strong> <br/>
                 Localization model in the saggital plane
               </td>
@@ -264,30 +275,11 @@
                 likelilangendijk.m <br/>
                 plotlangendijk.m <br/>
                 plotlikelilangendijk.m <br/>
-                exp_langendijk2002.m
+                exp_langendijk2002.m <br/>
+                data_langendijk2002.m
               </td>
               <td>
                 langendijk2002contribution
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <strong>Baumgartner model</strong> <br/>
-                Model for localization in saggital planes
-              </td>
-              <td>
-                baumgartner2013.m <br/>
-                plotbaumgartner2013.m <br/>
-                demo_baumgartner2013.m <br/>
-                exp_baumgartner2013.m <br/>
-                data_baumgartner2013.m
-              </td>
-              <td>
-                baumgartner2013assessment <br/>
-                baumgartner2012modelling <br/>
-                langendijk2002contribution <br/>
-                patterson1988efficient <br/>
-                dau1996qmeI
               </td>
             </tr>
             <tr>
@@ -306,6 +298,7 @@
                 exp_roenne2012.m <br/>
                 data_roenne2012.m <br/>
                 data_elberling2010.m <br/>
+                data_neely1988.m <br/>
                 roenne2012_elberling2010stim.mat <br/>
                 roenne2012_harte2009stim.mat
               </td>
@@ -313,6 +306,18 @@
                 roenne2012modeling <br/>
                 elberling2010evaluating <br/>
                 zilany2007representation
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Viemeister model</strong> <br/>
+                Leaky-integrator model
+              </td>
+              <td>
+                viemeister79.m
+              </td>
+              <td>
+                ???
               </td>
             </tr>
             <tr>
@@ -329,11 +334,40 @@
               </td>
             </tr>
 
-
+            <!-- ------------ B I N A U R A L ---------------------------- -->
             <tr class="info">
               <td><strong>Binaural models</strong></td>
               <td></td>
               <td></td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Breebart model</strong> <br/>
+                Computes the EI-cell representation of an input signal
+              </td>
+              <td>
+                breebart2001preproc.m <br/>
+                eicell.m
+              </td>
+              <td>
+                breebaart2001binaural
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Dietz model</strong> </br>
+                Binaural localization model using a count-comparison model to calculate the interaural phase difference.
+              </td>
+              <td>
+                dietz2011.m <br/>
+                dietz2011interauralfunctions.m <br/>
+                demo_dietz.m <br/>
+                exp_dietz2011.m <br/>
+                data_dietz2011.m
+              </td>
+              <td>
+                dietz2011auditory
+              </td>
             </tr>
             <tr>
               <td>
@@ -358,18 +392,16 @@
             </tr>
             <tr>
               <td>
-                <strong>Dietz model</strong> </br>
-                Binaural localization model using a count-comparison model to calculate the interaural phase difference.
+                <strong>May model</strong> <br/>
+                GMM-based localization model
               </td>
               <td>
-                dietz2011.m <br/>
-                dietz2011interauralfunctions.m <br/>
-                demo_dietz.m <br/>
-                exp_dietz2011.m <br/>
-                data_dietz2011.m
+                may2011.m <br/>
+                may2011neuraltransduction.m <br/>
+                demo_may2011.m
               </td>
               <td>
-                dietz2011auditory
+                may2011
               </td>
             </tr>
             <tr>
@@ -389,32 +421,8 @@
                 takanen2013periphery.m <br/>
                 takanen2013wbmso.m <br/>
                 demo_takanen2013.m <br/>
-                exp_takanen2013.m
-            <tr>
-              <td>
-                <strong>Breebart model</strong> <br/>
-                Computes the EI-cell representation of an input signal
-              </td>
-              <td>
-                breebart2001preproc.m <br/>
-                eicell.m
-              </td>
-              <td>
-                breebaart2001binaural
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <strong>May model</strong> <br/>
-                GMM-based localization model
-              </td>
-              <td>
-                may2011.m <br/>
-                may2011neuraltransduction.m <br/>
-                demo_may2011.m
-              </td>
-              <td>
-                may2011
+                exp_takanen2013.m <br/>
+                data_takanen2013.m
               </td>
             </tr>
             <tr>
@@ -427,12 +435,75 @@
                 ziegelwanger2013onaxis.m <br/>
                 ziegelwanger2013offaxis.m <br/>
                 plotziegelwanger2013.m <br/>
-                exp_ziegelwanger2013.m
+                exp_ziegelwanger2013.m <br/>
+                data_ziegelwanger2013.m
               </td>
               <td>
                 ziegelwanger2013
               </td>
             </tr>
+
+            <!-- ------------ H U M A N   D A T A ------------------------ -->
+            <tr class="info">
+              <td><strong>Human data</strong></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Glasberg & Moore</strong> <br/>
+                Notched-noise data for the ERB scale
+              </td>
+              <td>data_glasberg1990.m</td>
+              <td>
+                glasberg1990daf <br/>
+                moore1990auditory
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Goode</strong> <br/>
+                ...
+              </td>
+              <td>data_goode1994.m</td>
+              <td>goode1994nkf</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Joergensen</strong> <br/>
+                ...
+              </td>
+              <td>data_joergensen2011.m</td>
+              <td>joergensen2011predicting</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Pralong</strong> <br/>
+                Head phone data (compensation ???)
+              </td>
+              <td>data_pralong1996.m</td>
+              <td>pralong1996role</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>SII weights</strong>
+              </td>
+              <td>siiweightings.m</td>
+              <td>???</td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Zwicker</strong> <br/>
+                Data for the Bark scale
+              </td>
+              <td>data_zwicker1961.m</td>
+              <td>
+                zwicker1961saf <br/>
+                zwicker1999psychoacoustics
+              </td>
+            </tr>
+
+           
           </tbody>
         </table>
 
